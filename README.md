e09f344eb1bac09d9193e2c28470ca8ed88068e9
alexanderrohrauer <alex.rohrauer@outlook.de>
2025-04-05 14:33:40 +0200

    initial commit - README.md - added k3s ansible files

---
+++ README.md
@@ -0,0 +1,2 @@
+# SSH tunnel for Kubernetes
+`ssh lab04.ce.uni-linz.ac.at -L 6443:localhost:6443 -N`
---
+++ k3s-ansible/run_ansible.sh
@@ -0,0 +1,1 @@
+ansible-playbook playbooks/site.yml -i inventory.yml --user=students --become-password-file=~/.busempass --conn-pass-file=~/.busempass
